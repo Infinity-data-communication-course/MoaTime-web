31531ade5e9a6787999e4c474bc0da766669fcf4
chelsh <sinye2002@korea.ac.kr>
2024-12-08 00:50:51 +0900

    ✨ Add empty image

--- app/(main)/board/page.tsx
+++ app/(main)/board/page.tsx
@@ -124,102 +124,113 @@ export default function Board() {
       </div>
       <div className="flex flex-col w-full h-4/5 py-12 px-12 rounded-lg bg-neutral-100">
         <div className="space-y-5 overflow-y-scroll">
-          {myEvents.map((event, idx) => (
-            <div
-              key={idx}
-              className="w-full bg-neutral-200 rounded-lg h-20 px-6 flex flex-row justify-between items-center"
-            >
-              <div
-                className="flex flex-col w-full cursor-pointer"
-                onClick={() => {
-                  router.push(`/event/${event.id}`);
-                }}
-              >
-                <span className="text-2xl font-semibold">{event.title}</span>
-                <span>
-                  host: {event.hostName}
-                  {event.hostId === myId ? (
-                    <span className="bg-neutral-400 ml-1 text-xs text-white px-2 py-[2px] rounded-2xl">
-                      host
-                    </span>
-                  ) : null}
-                </span>
+          {myEvents.length === 0 ? (
+            <div className="w-full flex flex-col justify-center items-center *:text-center space-y-3">
+              <Image src="/empty.png" alt="empty" width={100} height={100} />
+              <div>
+                There are no events you participated in.
+                <br />
+                Create an event or participate in another event!
               </div>
-              {event.hostId === myId ? (
-                <svg
-                  data-slot="icon"
-                  fill="none"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                  aria-hidden="true"
-                  className="size-8 cursor-pointer hover:text-white"
-                  onClick={async () => {
-                    if (
-                      confirm(
-                        `Are you sure you want to delete the event: ${event.title}?`
-                      )
-                    ) {
-                      const res = await callAPI({
-                        url: `${process.env.NEXT_PUBLIC_SERVER_URL}/event/${event.id}/delete`!,
-                        method: "DELETE",
-                        isPrivate: true,
-                        accessToken: token,
-                      });
-                      if (res.status === 204) {
-                        setMyEvents((prev) =>
-                          prev.filter((e) => e.id !== event.id)
-                        );
-                      }
-                    }
-                  }}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                  ></path>
-                </svg>
-              ) : (
-                <svg
-                  data-slot="icon"
-                  fill="none"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                  aria-hidden="true"
-                  className="size-8 cursor-pointer hover:text-white"
-                  onClick={async () => {
-                    if (
-                      confirm(
-                        `Are you sure you want to leave the event: ${event.title}?`
-                      )
-                    ) {
-                      const res = await callAPI({
-                        url: `${process.env.NEXT_PUBLIC_SERVER_URL}/event/${event.id}/exit`!,
-                        method: "DELETE",
-                        isPrivate: true,
-                        accessToken: token,
-                      });
-                      if (res.status === 204) {
-                        setMyEvents((prev) =>
-                          prev.filter((e) => e.id !== event.id)
-                        );
-                      }
-                    }
-                  }}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9"
-                  />
-                </svg>
-              )}
             </div>
-          ))}
+          ) : (
+            myEvents.map((event, idx) => (
+              <div
+                key={idx}
+                className="w-full bg-neutral-200 rounded-lg h-20 px-6 flex flex-row justify-between items-center"
+              >
+                <div
+                  className="flex flex-col w-full cursor-pointer"
+                  onClick={() => {
+                    router.push(`/event/${event.id}`);
+                  }}
+                >
+                  <span className="text-2xl font-semibold">{event.title}</span>
+                  <span>
+                    host: {event.hostName}
+                    {event.hostId === myId ? (
+                      <span className="bg-neutral-400 ml-1 text-xs text-white px-2 py-[2px] rounded-2xl">
+                        host
+                      </span>
+                    ) : null}
+                  </span>
+                </div>
+                {event.hostId === myId ? (
+                  <svg
+                    data-slot="icon"
+                    fill="none"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                    className="size-8 cursor-pointer hover:text-white"
+                    onClick={async () => {
+                      if (
+                        confirm(
+                          `Are you sure you want to delete the event: ${event.title}?`
+                        )
+                      ) {
+                        const res = await callAPI({
+                          url: `${process.env.NEXT_PUBLIC_SERVER_URL}/event/${event.id}/delete`!,
+                          method: "DELETE",
+                          isPrivate: true,
+                          accessToken: token,
+                        });
+                        if (res.status === 204) {
+                          setMyEvents((prev) =>
+                            prev.filter((e) => e.id !== event.id)
+                          );
+                        }
+                      }
+                    }}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                    ></path>
+                  </svg>
+                ) : (
+                  <svg
+                    data-slot="icon"
+                    fill="none"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                    className="size-8 cursor-pointer hover:text-white"
+                    onClick={async () => {
+                      if (
+                        confirm(
+                          `Are you sure you want to leave the event: ${event.title}?`
+                        )
+                      ) {
+                        const res = await callAPI({
+                          url: `${process.env.NEXT_PUBLIC_SERVER_URL}/event/${event.id}/exit`!,
+                          method: "DELETE",
+                          isPrivate: true,
+                          accessToken: token,
+                        });
+                        if (res.status === 204) {
+                          setMyEvents((prev) =>
+                            prev.filter((e) => e.id !== event.id)
+                          );
+                        }
+                      }
+                    }}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9"
+                    />
+                  </svg>
+                )}
+              </div>
+            ))
+          )}
         </div>
       </div>
     </div>
